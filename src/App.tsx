e08a46734a7ed76d873dcb4096354670c940a10b
Ed Timmer <edtimmer@gmail.com>
2023-03-29 17:09:42 -0400

    added styled components and globalStyles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
-function App() { 
+import { AppContainer } from './App.css'
+
+const App = () => { 
   return (
-    <h1>Vite React Temlate</h1>
+    <AppContainer>
+      <h1>Vite React Temlate</h1>
+      <div>
+        Vite is a build tool that aims to provide a faster and
+        leaner development experience for modern web projects
+      </div>
+    </AppContainer>
   )
 }
 
