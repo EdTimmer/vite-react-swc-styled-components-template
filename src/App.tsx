9136f52ce4df957f414e2b73c199965ac3235d11
Ed Timmer <edtimmer@gmail.com>
2023-03-30 11:42:18 -0400

    stylistic changes to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
 import { AppContainer } from './App.css'
 
-const App = () => { 
+const App = () => {
   return (
     <AppContainer>
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
       <h1>Vite React Temlate</h1>
       <div>
-        Vite is a build tool that aims to provide a faster and
-        leaner development experience for modern web projects
+        Vite is a build tool that aims to provide a faster and leaner development experience for modern web projects
       </div>
     </AppContainer>
   )
